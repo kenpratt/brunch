53caa85a3e6325fda95a23d4d4fc77d0ff01eb37
Ken Pratt <ken@kenpratt.net>
2013-02-27 18:24:17 -0800

    Check to see if coffee/mocha are installed as siblings when running post-install hooks after git install.

--- setup.js
+++ setup.js
@@ -8,8 +8,16 @@ var mode = process.argv[2];
 
 var fsExists = fs.exists || sysPath.exists;
 
-var getBinaryPath = function(binary) {
-  return sysPath.join('node_modules', '.bin', binary);
+var getBinaryPath = function(binary, callback) {
+  var path = sysPath.join('node_modules', '.bin', binary);
+  fsExists(path, function(exists) {
+    if (exists) return callback(path);
+    path = sysPath.join('..', '.bin', binary);
+    fsExists(path, function(exists) {
+      if (exists) return callback(path);
+      callback(binary); // hope it's on the PATH
+    });
+  });
 };
 
 var execute = function(path, params, callback) {
@@ -39,7 +47,7 @@ switch (mode) {
   // Remove `.postinstall` script to prevent stupid npm bugs.
   case 'prepublish':
     togglePostinstall(false);
-    execute(getBinaryPath('coffee'), '-o lib/ src/');
+    getBinaryPath('coffee', function(coffee) { execute(coffee, '-o lib/ src/'); });
     break;
 
   // Bring back `.postinstall` script.
@@ -51,15 +59,14 @@ switch (mode) {
   case 'postinstall':
     fsExists(sysPath.join(__dirname, 'lib'), function(exists) {
       if (exists) return;
-      execute(getBinaryPath('coffee'), '-o lib/ src/');
+      getBinaryPath('coffee', function(coffee) { execute(coffee, '-o lib/ src/'); });
     });
     break;
 
   // Run tests.
   case 'test':
-      execute(
-        getBinaryPath('mocha'),
-        '--compilers coffee:coffee-script --require test/common.js --colors'
-      );
+    getBinaryPath('mocha', function(mocha) {
+      execute(mocha,'--compilers coffee:coffee-script --require test/common.js --colors');
+    });
     break;
 }
